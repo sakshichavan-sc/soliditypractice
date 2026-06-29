93e1551916aecdd9da3afa49e529bc2091764c3e
sakshi c <sakshichavan.sn@gmail.com>
2026-06-29 23:23:12 +0530

    initial commit

---
+++ Structure.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.20;
+import "@openzeppline/contracts/access/ownable.sol";
+contract studentRegistery {
+    uint public studentCount;
+    constructor(){}
+    function registerStudent()public{}
+
+}
